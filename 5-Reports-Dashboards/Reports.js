61c4e45a4f1bc59bedc5a288d9b35b7097e31298
MOHAMED <98075987+mohamedshneshn@users.noreply.github.com>
2024-10-12 15:03:10 -0700

    Third

---
+++ 5-Reports-Dashboards/Reports.js
@@ -0,0 +1,43 @@
+/*
+
+1. What is a report in Salesforce?
+------------------------------------
+- A report in Salesforce is a list of records that meet the criteria you define. 
+- It can be displayed in a specific format, such as a table or chart.
+- Reports can be used to analyze data, track performance, and make informed decisions.
+
+- Every report is stored in a folder, which determines who can view and edit it.
+- Reports can Public, Hidden, or Shared.
+- we can controll access to reports based on roles, profiles, public groups, and territories.
+- Reports can be scheduled to run at specific times and emailed to users.
+
+
+
+2. What are the different types of reports in Salesforce?
+---------------------------------------------------------
+- There are four main types of reports in Salesforce:
+    1- Tabular Reports: 
+        - displays data in a simlpe list of rows with columns (table format).
+        
+        
+    2- Summary Reports: 
+        - Group data by columns, and provide subtotals.
+
+    3- Matrix Reports: 
+        - displays data in a grid format with both rows and columns.
+        - used when you want to see data summarized in both directions.
+    4- Joined Reports: 
+        - used to combine data from multiple report types into a single report.
+        
+
+
+
+
+
+
+3. What is a dashboard in Salesforce?
+--------------------------------------
+- used to display multiple reports side by side on a single page.
+
+
+*/
